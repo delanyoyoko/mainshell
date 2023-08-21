7d162e5f05e31f0fcf22855d92a1b427adb6fab1
delanyo agbenyo <delanyodevio@gmail.com>
2023-08-21 04:05:56 +0000

    Update builtin.c funcx

--- builtin.c
+++ builtin.c
@@ -1,98 +1,100 @@
-#include "shell.h"
+#include "mainshell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
+ * shell_exit - helps to exits the shell
+ * @infoval: struct involving arguments. Used to maintain
  * constant function prototype.
- * Return: exits with a given exit status
- * (0) if info.argv[0] != "exit"
+ * Return: exits the shell with a given exit status code
+ * (0) if info.argv[0] is not equal to  "exit"
  */
-int _myexit(info_t *info)
+int shell_exit(info_type *infoval)
 {
-	int exitcheck;
+	int check_exit;
 
-	if (info->argv[1]) /* If there is an exit arguement */
+	if (infoval->argv[1])
 	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		check_exit = atoi_error(infoval->argv[1]);
+		if (check_exit == -1)
 		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
+			infoval->status = 2;
+			print_err(infoval, "Illegal number: ");
+			puts_error(infoval->argv[1]);
+			putchar_error('\n');
 			return (1);
 		}
-		info->err_num = _erratoi(info->argv[1]);
+		infoval->err_num = atoi_error(infoval->argv[1]);
 		return (-2);
 	}
-	info->err_num = -1;
+	infoval->err_num = -1;
 	return (-2);
 }
 
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
+ * shell_cd - changes the current working directory or folder
+ * @infoval: struct involving arguments. Used to maintain
  * constant function prototype.
  * Return: Always 0
  */
-int _mycd(info_t *info)
+int shell_cd(info_type *infoval)
 {
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
+	char *shll;
+	char *fold;
+	char buffered[1024];
+	int ch_dir_ret;
 
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
+	shll = getcwd(buffered, 1024);
+	if (!shll)
+		_puts("Failed to get current working directory.\n");
+	if (!infoval->argv[1])
 	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		fold = get_env(infoval, "HOME=");
+		if (!fold)
+			ch_dir_ret = 
+				chdir((fold = get_env(infoval, "PWD=")) ? fold : "/");
 		else
-			chdir_ret = chdir(dir);
+			ch_dir_ret = chdir(fold);
 	}
-	else if (_strcmp(info->argv[1], "-") == 0)
+	else if (string_cmp(infoval->argv[1], "-") == 0)
 	{
-		if (!_getenv(info, "OLDPWD="))
+		if (!get_env(infoval, "OLDPWD="))
 		{
-			_puts(s);
+			_puts(shll);
 			_putchar('\n');
 			return (1);
 		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+		_puts(get_env(infoval, "OLDPWD=")), _putchar('\n');
+		ch_dir_ret = 
+			chdir((fold = get_env(infoval, "OLDPWD=")) ? fold : "/");
 	}
 	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
+		ch_dir_ret = chdir(infoval->argv[1]);
+	if (ch_dir_ret == -1)
 	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
+		print_err(infoval, "can't cd to dir");
+		puts_error(infoval->argv[1]), putchar_error('\n');
 	}
 	else
 	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
+		set_shell_env(infoval, "OLDPWD", get_env(infoval, "PWD="));
+		set_shell_env(infoval, "PWD", getcwd(buffered, 1024));
 	}
 	return (0);
 }
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
+ * shell_help - changes the current working directory of the process
+ * @infoval: struct involving arguments. Used to maintain
  * constant function prototype.
  * Return: Always 0
  */
-int _myhelp(info_t *info)
+int shell_help(info_type *infoval)
 {
-	char **arg_array;
+	char **arg_list;
 
-	arg_array = info->argv;
+	arg_list = infoval->argv;
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
+		_puts(*arg_list);
 	return (0);
 }
 
