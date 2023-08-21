2ef834315538be39d31d27c8176ae50815197222
delanyo agbenyo <delanyodevio@gmail.com>
2023-08-21 15:07:54 +0000

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,74 +1,76 @@
-#include "shell.h"
+#include "mainshell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
+ **copy_string - copies a string to memory
+ *@des: the destination string to be copied to
+ *@source: the source string to copy
+ *@num: the number of characters to be copied
  *Return: the concatenated string
  */
-char *_strncpy(char *dest, char *src, int n)
+char *copy_string(char *des, char *source, int num)
 {
-	int i, j;
-	char *s = dest;
+	int idx;
+	int jdx;
+	char *strng = des;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
+	idx = 0;
+	while (source[idx] != '\0' && idx < num - 1)
 	{
-		dest[i] = src[i];
-		i++;
+		des[idx] = source[idx];
+		idx++;
 	}
-	if (i < n)
+	if (idx < num)
 	{
-		j = i;
-		while (j < n)
+		jdx = idx;
+		while (jdx < num)
 		{
-			dest[j] = '\0';
-			j++;
+			des[jdx] = '\0';
+			jdx++;
 		}
 	}
-	return (s);
+	return (strng);
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ **cat_string - to concatenates two strings
+ *@_first: the first string
+ *@_second: the second string
+ *@nun: the amount of bytes to be max used
+ *Return: the combined concatenated string
  */
-char *_strncat(char *dest, char *src, int n)
+char *cat_string(char *_first, char *_second, int n)
 {
-	int i, j;
-	char *s = dest;
+	int idx;
+	int jdx;
+	char *strng = _first;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
+	idx = 0;
+	jdx = 0;
+	while (_first[idx] != '\0')
+		idx++;
+	while (_second[jdx] != '\0' && jdx < n)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		_first[idx] = _second[jdx];
+		idx++;
+		jdx++;
 	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+	if (jdx < n)
+		_first[idx] = '\0';
+	return (strng);
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ **char_string - to locates a character in a string
+ *@strng: the string to be parsed
+ *@cter: the character to look for
+ *Return: (strng) a pointer to the memory area of strng
  */
-char *_strchr(char *s, char c)
+char *char_string(char *strng, char cter)
 {
 	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
+		if (*strng == cter)
+			return (strng);
+	} while (*strng++ != '\0');
 
 	return (NULL);
 }
